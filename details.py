d72aefcbf4ad72c07a1429709171974767c35c53
piyush4014 <140847217+piyush4014@users.noreply.github.com>
2023-12-02 20:52:00 +0530

    Update details.py

--- details.py
+++ details.py
@@ -2,5 +2,5 @@ api_id = 29410389
 api_hash = "0c716764715886f6641477ffbb63e1ee"
 bot_token = "6891181230:AAExgbCfEqc-p9sC54_4rjQtwuw1Q1ya0h8"
 sudo_groups = -1001999673737
-sudo_user = 6899244704
+sudo_user = 6899244704,5442868227
 
