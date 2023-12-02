4307c111bfff0486f8fea45a8112b502c93280b4
piyush4014 <140847217+piyush4014@users.noreply.github.com>
2023-12-02 20:46:47 +0530

    Update main.py

--- main.py
+++ main.py
@@ -39,7 +39,7 @@ import os
 bot = Client(
     "bot",
     bot_token=os.environ.get("BOT_TOKEN"),
-    api_id=int(os.environ.get(API_ID)),
+    api_id=os.environ.get("API_ID"),
     api_hash=os.environ.get("API_HASH")
 )
 auth_users = [ int(chat) for chat in os.environ.get("AUTH_USERS").split(",") if chat != '']
