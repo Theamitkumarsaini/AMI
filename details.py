5fc42ae4d77194db9ddeb51036deb592339836d4
Theamitkumarsaini <145679072+Theamitkumarsaini@users.noreply.github.com>
2024-02-10 08:37:55 +0000

    Update details.py

--- details.py
+++ details.py
@@ -1,6 +1,6 @@
 api_id = 20544260
 api_hash = "a0b00461d3fba22aa186fa648d77787e"
-bot_token = "6940355682:AAEaTbv9tLz7ZKOEcMJ1HBioIP2goQOdQSY"
-sudo_groups = -4095451971
+bot_token = "6968574821:AAH0ZPX1aRJfyE0hjn8iJermZcU8hZiGDJI"
+sudo_groups = -1002117596251
 sudo_user = 6389388334
 
