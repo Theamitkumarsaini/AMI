cd699e47174119caa6f5ba07993c3f21de32c8c4
Theamitkumarsaini <145679072+Theamitkumarsaini@users.noreply.github.com>
2024-02-25 11:07:47 +0530

    Update details.py

--- details.py
+++ details.py
@@ -1,6 +1,21 @@
-api_id = 20544260
-api_hash = "a0b00461d3fba22aa186fa648d77787e"
-bot_token = "6968574821:AAH0ZPX1aRJfyE0hjn8iJermZcU8hZiGDJI"
-sudo_groups = -1002117596251
-sudo_user = 6389388334
-auth_users = 6389388334
+import os
+
+API_ID = API_ID = 20544260
+
+API_HASH = os.environ.get("API_HASH", "a0b00461d3fba22aa186fa648d77787e")
+
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "6940355682:AAEaTbv9tLz7ZKOEcMJ1HBioIP2goQOdQSY")
+
+PASS_DB = int(os.environ.get("PASS_DB", "721"))
+
+OWNER = int(os.environ.get("OWNER", 6389388334))
+
+LOG = -1002051259332
+
+try:
+    ADMINS=[]
+    for x in (os.environ.get("ADMINS", "6389388334").split()):
+        ADMINS.append(int(x))
+except ValueError:
+        raise Exception("Your Admins list does not contain valid integers.")
+ADMINS.append(OWNER)
