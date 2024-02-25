cf4aeb37f6e7d3b8025d92439ebc2942fe629503
Theamitkumarsaini <145679072+Theamitkumarsaini@users.noreply.github.com>
2024-02-25 10:32:25 +0530

    Update details.py

--- details.py
+++ details.py
@@ -3,4 +3,4 @@ api_hash = "a0b00461d3fba22aa186fa648d77787e"
 bot_token = "6968574821:AAH0ZPX1aRJfyE0hjn8iJermZcU8hZiGDJI"
 sudo_groups = -1002117596251
 sudo_user = 6389388334
-
+auth_users = 6389388334
